6bd6ba804e9edaee1077e1bf52f4c4c001b1fa8d
Muhammad Uzair <muhammad.uzair@constellationhb.com>
2024-04-22 01:22:17 +0500

    explain git commands and local variables

---
+++ src/practice_22_04_2024/DataTypes.java
@@ -0,0 +1,15 @@
+package practice_22_04_2024;
+
+public class DataTypes
+{
+    public static void main(String[] args)
+    {
+        //local variables
+        //declare and initialize
+
+        int num = 5;
+        System.out.println("num is " + num);
+
+    }
+
+}
